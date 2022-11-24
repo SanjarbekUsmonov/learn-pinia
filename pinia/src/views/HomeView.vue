108bf41b5e47623f3b1cdb843b1939c0d85bb2e9
SanjarbekUsmonov <noobpusk@gmail.com>
2022-11-24 13:15:39 +0500

    first commit

---
+++ pinia/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div class="count">
+      0
+    </div>
+    <div class="buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style  scoped>
+  .count{
+    font-size: 60px;
+    margin: 20px;
+  }
+  .buttons button{
+    font-size: 40px;
+    margin: 20px;
+  }
+</style>
